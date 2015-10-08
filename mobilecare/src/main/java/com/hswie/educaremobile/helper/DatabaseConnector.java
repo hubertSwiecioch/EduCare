5b4eb4fbf9266129858b3673e3f92e8018f7adba
hswie <hswiecioch@hotmail.com>
2015-10-09 00:55:31 +0200

    first database connection

--- mobilecare/src/main/java/com/hswie/educaremobile/helper/DatabaseConnector.java
+++ mobilecare/src/main/java/com/hswie/educaremobile/helper/DatabaseConnector.java
@@ -14,30 +14,28 @@ import java.sql.Statement;
  */
 public class DatabaseConnector {
 
-    private static final String HOSTNAME = "192.168.1.201";
+    private static final String HOSTNAME = "192.168.0.110";
     private static final String PORT = "3306";
-//    private static final String USERNAME = "b6fd11d286cd1b";
-//    private static final String PASSWORD = "a8f6b4a1";
+    private static final String USERNAME = "hswie";
+    private static final String PASSWORD = "hswie";
     private static final String DATABASE = "educare";
     private static final String URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/" + DATABASE;
     ;
 
     private static final String TAG = "DatabaseConnector";
 
-    public String connectToDatabase(String username, String password) {
+    public String connectToDatabase(String login, String password) {
 
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Log.d(TAG, username + " " + password);
-            Connection con = DriverManager.getConnection(URL, username, password);
+            Log.d(TAG, login + " " + password);
+            Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             /* System.out.println("Database connection success"); */
 
             String result = "";
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select Password from nurse where Login = '" + username + "'");
-
-            Log.d(TAG, "Result" + result);
+            ResultSet rs = st.executeQuery("select Password from nurse where Login = '" + login + "'");
 
             while(rs.next()) {
                 result = rs.getString("Password");
@@ -45,6 +43,7 @@ public class DatabaseConnector {
                 return  result;
 
             }
+
         }
         catch(Exception e) {
             e.printStackTrace();
