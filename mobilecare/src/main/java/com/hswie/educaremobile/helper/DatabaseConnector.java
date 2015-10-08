e157aaf8658117c094b9151f8019422f13679e5a
hswie <hswiecioch@hotmail.com>
2015-10-08 21:39:09 +0200

    initial commit

---
+++ mobilecare/src/main/java/com/hswie/educaremobile/helper/DatabaseConnector.java
@@ -0,0 +1,27 @@
+package com.hswie.educaremobile.helper;
+
+
+
+
+/**
+ * Created by hswie on 08.10.2015.
+ */
+public class DatabaseConnector {
+
+    private static final String HOSTNAME = "eu-cdbr-azure-west-c.cloudapp.net";
+    private static final String PORT = "3306";
+    private static final String USERNAME = "b6fd11d286cd1b";
+    private static final String PASSWORD = "a8f6b4a1";
+    private static final String CONNECTION_STRING = "Database=EduCare;Data Source=eu-cdbr-azure-west-c.cloudapp.net;" +
+                                                    "User Id=b6fd11d286cd1b;Password=a8f6b4a1";
+
+
+
+
+    public void connectToDatabase() {
+
+
+    }
+
+}
+
