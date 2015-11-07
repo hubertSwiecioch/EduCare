2f351615268065f30d6fe0125e4551b84cb18a8b
hswie <hswiecioch@hotmail.com>
2015-11-07 22:31:18 +0100

    memory problem (fix?), work on add resident activity

--- mobilecare/src/main/java/com/hswie/educaremobile/carer/AddResidentActivity.java
+++ mobilecare/src/main/java/com/hswie/educaremobile/carer/AddResidentActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import com.hswie.educaremobile.R;
 
@@ -18,7 +19,10 @@ import java.util.Locale;
 public class AddResidentActivity extends AppCompatActivity {
 
     private Calendar myCalendar = Calendar.getInstance();
-    private EditText dateOfAdoption, birthDate;
+    private EditText dateOfAdoption, birthDate, getPhoto;
+    private Switch switchImage;
+
+    private int currentEditDate = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +32,22 @@ public class AddResidentActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        initView();
+
+
+    }
+
+    private void initView(){
+
         dateOfAdoption = (EditText) findViewById(R.id.date_of_adoption);
         birthDate = (EditText) findViewById(R.id.birth_date);
 
+        switchImage = (Switch) findViewById(R.id.imageSwitch);
+        switchImage.setTextOn(getText(R.string.switch_camera));
+        switchImage.setTextOff(getText(R.string.switch_gallery));
+
         dateOfAdoption.setOnClickListener(setDateListener);
         birthDate.setOnClickListener(setDateListener);
-
     }
 
     private View.OnClickListener setDateListener= new View.OnClickListener() {
@@ -44,7 +58,7 @@ public class AddResidentActivity extends AppCompatActivity {
             new DatePickerDialog(AddResidentActivity.this, date, myCalendar
                     .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                     myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-
+            currentEditDate = v.getId();
         }
     };
 
@@ -69,7 +83,11 @@ public class AddResidentActivity extends AppCompatActivity {
         String myFormat = "MM/dd/yy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
 
+        if (currentEditDate == dateOfAdoption.getId())
         dateOfAdoption.setText(sdf.format(myCalendar.getTime()));
+
+        if (currentEditDate == birthDate.getId())
+            birthDate.setText(sdf.format(myCalendar.getTime()));
     }
 
 
